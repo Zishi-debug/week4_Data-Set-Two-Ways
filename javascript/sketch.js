5d9b870ad0b5d0687c01f4029599dcfab545478b
Zishi-debug <tuzishizhenkeai@gmail.com>
2023-02-09 18:44:17 +0000

    added
    my sanity gonna be 100.

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,23 +1,26 @@
-//no animation / interaction chart
-//if you want to use animation or create a loading state look at the cat fact example from last week 
-// use a boolean to control when your data is loaded
+//this chart shows  how my sanity changes in this week. Coding makes me insane.
+// I am deadge >_<
 
 
-let breakfast;
+let Sanity;
+let amplitude;
+let num;//how many vertex on the line
+let canvas;
 
 function setup() {
-  createCanvas(500, 500);
+  canvas= createCanvas(windowWidth,windowHeight);
+  canvas.parent("sketch-container");
+  
+  num= 10;
 
   //no animation / interaction chart
   noLoop();
 
-  fetch("./json/breakfast.json").then(function(response) {
+  fetch("./json/Youxy Sanity.json").then(function(response) {
     return response.json();
   }).then(function(data) {
-
-    console.log(data);
     
-    breakfast = data.breakfast;
+    Sanity = data.sanity;
 
     //using no Loop? you can just call your function once the data is loaded
     drawChart();
@@ -29,40 +32,77 @@ function setup() {
 }
 
 function draw() {
-  background(200);
-
+  background(0);
+  drawChart();
 }
 
 function drawChart(){
 
   // Compute maximum amount (for normalization)
   let maxval = 0; 
-  for (let i=0; i<breakfast.length; i++) {
-    if ( breakfast[i].amount > maxval) {
-      maxval = breakfast[i].amount;
+  for (let i=0; i<Sanity.length; i++) {
+    if ( Sanity[i].Hp > maxval) {
+      maxval = Sanity[i].Hp;
     }
   }
 
-  let spacing = 5;//spacing between the bars
+  let spacing = 15;//spacing between the bars
   // Display chart
-  for (let i=0; i<breakfast.length; i++) {
+  for (let i=0; i<Sanity.length; i++) {
 
-    let item = breakfast[i];
+    let item = Sanity[i];
     
-    let rWidth = width/(breakfast.length+2); //add 2 so there is space either side of the chart
-    let rX = map(i, 0, breakfast.length, rWidth, width-rWidth); //map range includes the space on either side
+    let rWidth = width/(Sanity.length+2); //add 2 so there is space either side of the chart
+    let rX = map(i, 0, Sanity.length, rWidth, width-rWidth); //map range includes the space on either side
     let rY = height-rWidth; 
-    let rHeight = 0-map(item.amount, 0, maxval, 0, height-(rWidth*2)); // map height so spacing on top + bottom match side spacing 
-    
-    console.log(rHeight);
-    
-    noStroke(); 
-    fill(item.color);
-    rect(rX+spacing/2, rY, rWidth-spacing, rHeight); 
+    let rHeight = 0-map(item.Hp, 0, maxval, 0, height-(rWidth*2)); // map height so spacing on top + bottom match side spacing 
 
-    fill(0); 
+    //draw line here
+  
+    push();
+    strokeWeight(2);
+    noFill();
+    stroke(map(item.sanity,0,100,50,255),0,0);
+     beginShape();
+      
+       circle(rX+spacing/2,rY,5);
+       translate(rX+spacing/2,rY+rHeight)
+        
+         circle(0,-rHeight,5);
+         
+
+         vertex(0,-rHeight);
+         vertex(0,-rHeight);
+           // draw the wave
+           for(let j=0;j<num;j++){
+             let f= j*2+i*2;
+             console.log("f="+f);
+             let vX= j*(rWidth/num);
+                 amplitude= map(item.sanity,0,100,0,200);
+                 eyePosition= map(item.Hp,0,100,0,200);
+                 eyeSize= map(item.sanity,0,100,0,50);
+                 ellipse(10,amplitude,8,eyeSize);//the eye_L
+                 ellipse(rWidth-spacing-windowWidth/30,amplitude,5,eyeSize);//the eye_R
+             let vY= amplitude * noise(f);
+              curveVertex(vX,vY);
+              console.log("vY="+vY);
+            }
+
+         
+          vertex(rWidth-spacing,-rHeight);
+          vertex(rWidth-spacing,-rHeight);
+
+          circle(rWidth-spacing,-rHeight,5);
+     endShape();
+    pop();
+  
+    //the date
+    fill(255); 
     textAlign(CENTER, TOP); 
-    text(item.ingredient, rX+rWidth/2-1, rY+10);
+    text(item.date, rX+rWidth/2-1, rY+20);
   }  
 
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
